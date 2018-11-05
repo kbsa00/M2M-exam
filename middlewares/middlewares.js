84cc6e227bc9803389cca196f6deced4364ef2bc
Khalid B.S <khalid_bs@hotmail.com>
2018-11-05 20:13:23 +0100

    heroku fix

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -24,13 +24,4 @@ module.exports = (app) => {
     mongoose.connect(keys.MONGO_URI, {
         useNewUrlParser: true
     });
-
-    if (process.env.NODE_ENV === 'production') {
-
-        app.use(express.static('client/build'))
-        app.get('*', (req, res) => {
-            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-        })
-
-    }
 };
--- server.js
+++ server.js
@@ -7,7 +7,17 @@ require('./routes/routes')(app);
 require('./model/user');
 require('./model/analytics');
 
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+
+}
+
 
 app.listen(PORT);
 console.log(`Listening to Port ${PORT}`); 
