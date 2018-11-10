20b6b336115e97161be516ca8526cf7fa35668c1
Khalid B.S <khalid_bs@hotmail.com>
2018-11-10 03:12:21 +0100

    Header is fixed with routing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,22 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'; 
 
 export default function Header() {
   return (
     <div>
       <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-       <h1 className="header navbar-brand">M2M-Eksamen</h1>
+       <Link to="/"><h1 className="header navbar-brand">M2M-Eksamen</h1></Link>
         <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              
+            <Link to="/live" style={{ textDecoration: 'none' }}>
+              Livefeed
+            </Link>
             </li>
 
             <li className="nav-item active">
-               
-            </li>
-
-            <li className="nav-item active">
-                
+            <Link to="/#" style={{ textDecoration: 'none' }}>
+              Overview
+            </Link>
             </li>
         </ul>
       </nav>
